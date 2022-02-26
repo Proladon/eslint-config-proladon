af8e291ceabe63b18a99040f1022b55a9f0d5fed
Proladon <Proladon@gmail.com>
2022-02-26 20:56:37 +0800

    1.0.6

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ module.exports = {
     'vue2-recommended': require('./lib/vue2-recommended'),
     'vue3-essential': require('./lib/vue3-essential'),
     'vue3-recommended': require('./lib/vue3-recommended'),
+    eslintTypescript: require('./lib/eslintTypescript'),
   },
 }
